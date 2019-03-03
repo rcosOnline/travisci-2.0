4f46e9e744c66c420b69ad1f0ad94d9fd6fb57ab
bmrmozz <bmrmozz@gmail.com>
2019-03-03 15:09:09 +0200

    test

--- testapi2/Controllers/ValuesController.cs
+++ testapi2/Controllers/ValuesController.cs
@@ -11,7 +11,7 @@ namespace testapi2.Controllers {
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get() {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "brandon" };
         }
 
         // GET api/values/5
